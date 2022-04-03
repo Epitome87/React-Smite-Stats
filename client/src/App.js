a28c3e58f21dc15fbd2fd867fd5af42433853720
Matthew McGrath <rubywep@hotmail.com>
2022-04-02 18:22:15 -0700

    Add Route handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import MatchSummary from "./components/match_summary/MatchSummary.js"
-import game from "./components/game.json"
+import MatchSummary from './components/match_summary/MatchSummary.js';
+import MatchesPage from './components/MatchesPage';
+import ProfilePage from './components/ProfilePage';
+import HomePage from './components/HomePage';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <div className='App'>
-      <MatchSummary game={game}/>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/profile/:id' element={<ProfilePage />} />
+          <Route path='/matches' element={<MatchesPage />} />
+          <Route path='/matches/:id' element={<MatchSummary />} />
+        </Routes>
+        {/* Some sort of Footer */}
+      </Router>
     </div>
   );
 }
